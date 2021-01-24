110e90aae15f2771eded47b9422d3e9b6e4cffbb
AvrahamKahan123 <avrahamkahan123@gmail.com>
2021-01-24 23:20:41 +0200

    removed replaced prior code

--- src/MPRViewer.py
+++ src/MPRViewer.py
@@ -4,7 +4,6 @@ import numpy as np
 from vtk import vtkImageData
 from vtk.util import numpy_support
 import getMPR
-from MRISequenceViewer import PlaneViewerQT
 from MPRViwerProp import viewerLogic
 from PointCollection import PointCollection
 
@@ -138,55 +137,6 @@ class View:
             self.renderer.AddActor(currentPolygonActor)
         self.presentPoints()
 
-
-        """
-        picking_z = 0
-        PointIdImage = np.array([pickedCoordinates[0],pickedCoordinates[1],picking_z]) #x,y,z,sliceID
-        PointIdImage = PointIdImage.tolist()
-
-        # extent_x = self.MPRViewerProperties.MPR_M.shape[0]*self.MPRViewerProperties.delta
-        # extent_y = self.MPRViewerProperties.MPR_M.shape[1]*self.MPRViewerProperties.delta
-        print(self.MPRViewerProperties.MPR_M.shape)
-        x_pixel = np.int(pickedCoordinates[0]//self.MPRViewerProperties.delta)
-        y_pixel = np.int(pickedCoordinates[1]//self.MPRViewerProperties.delta)
-        PointsIndex =  [x_pixel,y_pixel]
-        # PointIdImage_Pixel = %[0,0] - left, bottom corner
-        if PointIdImage in self.PickingPointsIm:
-            print("need to remove point")
-            i = self.PickingPointsIm.index(PointIdImage)
-            actor = self.polygonActorList[i]
-            self.renderer.RemoveActor(actor)
-            self.renderWindow.Render()
-            self.PickingPointsIm.remove(self.PickingPointsIm[i])
-            self.PickingPointsIndex.remove(self.PickingPointsIndex[i])
-            self.polygonList.remove(self.polygonList[i])
-            self.polygonActorList.remove(self.polygonActorList[i])
-        else:
-            self.PickingPointsIm.append(PointIdImage)
-            self.PickingPointsIndex.append(PointsIndex)
-            print(self.PickingPointsIm)
-            print(self.PickingPointsIndex)
-            # print(self.PickingPointVTK)
-            polygon = vtk.vtkRegularPolygonSource()
-            polygon.SetCenter(pickedCoordinates)
-            polygon.SetRadius(1)
-            polygon.SetNumberOfSides(15)
-            # polygon.SetNormal(1, 2, 3)
-            polygon.GeneratePolylineOff()
-            polygon.GeneratePolygonOn()
-            polygonMapper = vtk.vtkPolyDataMapper()
-            polygonMapper.SetInputConnection(polygon.GetOutputPort())
-            polygonActor = vtk.vtkActor()
-            polygonActor.SetMapper(polygonMapper)
-            polygonActor.GetProperty().SetColor(1, 0, 0)
-            polygonActor.GetProperty().SetAmbient(1)
-
-            self.polygonActorList.append(polygonActor)
-            self.polygonList.append(polygon)
-            self.renderer.AddActor(polygonActor)
-
-        self.presentPoints()
-        """
     def presentPoints(self):
         for point in self.lengthPoints.points:
             polygon = point.polygon
