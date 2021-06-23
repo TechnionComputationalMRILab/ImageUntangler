140a65d9e80c531ded6e44acd26c44c94314fd26
Angeleene (Zemmy) Ang <zemmy.ang@gmail.com>
2021-06-23 16:16:04 +0300

    merging

--- src/Model/Readers/DICOMReader_test.py
+++ src/Model/Readers/DICOMReader_test.py
@@ -4,7 +4,7 @@ from DICOMReader import DICOMReader
 absolute_path = "C:\\Users\\vardo\\OneDrive\\Documents\\Github\\ImageUntangler\\internal_data\\MRI_Data\\enc_files\\"
 test_dicom_reader = DICOMReader(absolute_path)
 
-# print(test_dicom_reader.get_sequence_list())
+print(test_dicom_reader.get_sequence_list())
 
 
 def test_caching():
