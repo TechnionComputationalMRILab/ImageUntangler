45d93a6078e1a6e53534df27d275f178e04d23ad
Angeleene Ang <ang.a@technion.ac.il>
2021-08-26 13:14:20 +0300

    update version number

--- MRICenterline/Points/PointCollection2/__init__.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from typing import List
-import vtkmodules.all as vtk
-
-
-class PointCollection2(vtk.vtkActorCollection):
-    def __init__(self):
-        super().__init__()
-        pass
